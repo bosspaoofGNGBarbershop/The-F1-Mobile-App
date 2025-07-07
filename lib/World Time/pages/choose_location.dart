87ae49c114b63127a02c04f45ed20baa15571576
bosspaoofGNGBarbershop <johnpaolo.creus@g.batstate-u.edu.ph>
2025-07-07 11:55:52 +0800

    this is my activity july 7

---
+++ lib/World Time/pages/choose_location.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('Build function ran');
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.red[700],
+        title: Text('Choose Location',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Text('choose location screen'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,13 @@
 import 'package:flutter/material.dart';
+import 'World Time/pages/choose_location.dart';
+import 'World Time/pages/home.dart';
+import 'World Time/pages/loading.dart';
 
-void main() {
-  runApp(MyApp());}
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('The F1 Mobile App'),
-          backgroundColor: Colors.blue,
-        ),
-        body: SafeArea(
-          child: Center(
-            child: Container(
-              width: 300, // adjust as needed
-              padding: EdgeInsets.all(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Name:',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'John Creus',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        'Age:',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        '24 years old',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        'Gender:',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'Male',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/Location': (context) => ChooseLocation(),
+  },
+));
